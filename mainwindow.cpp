1f9eeb2f1957ff77f0c357e477fc891eb58e207c
vglvproc <vglv.work@gmail.com>
2020-03-03 00:34:36 +0400

    Some very basic interface

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,27 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QTreeWidgetItem* sortNode = new QTreeWidgetItem(ui->twMain);
+    sortNode->setText(0, QString("Алгоритмы сортировки"));
+
+    QTreeWidgetItem* bogosortNode = new QTreeWidgetItem(sortNode);
+    bogosortNode->setText(0, QString("Случайная сортировка"));
+
+    QTreeWidgetItem* bubbleNode = new QTreeWidgetItem(sortNode);
+    bubbleNode->setText(0, QString("Пузырьковая сортировка"));
+
+    QTreeWidgetItem* insertionNode = new QTreeWidgetItem(sortNode);
+    insertionNode->setText(0, QString("Сортировка вставками"));
+
+    QTreeWidgetItem* shellNode = new QTreeWidgetItem(sortNode);
+    shellNode->setText(0, QString("Сортировка Шелла"));
+
+    QTreeWidgetItem* treeNode = new QTreeWidgetItem(sortNode);
+    treeNode->setText(0, QString("Сортировка двоичным деревом"));
+
+    ui->twMain->setHeaderLabel(QString("Алгоритмы и структуры данных"));
+    ui->twMain->setFont(QFont("Times", 12));
+    ui->twMain->expandAll();
 }
 
 MainWindow::~MainWindow()
